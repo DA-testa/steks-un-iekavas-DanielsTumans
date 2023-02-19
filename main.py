a7d5fdcc645945caa636cc9cc0d43efa98ec92dd
DanielsTumans <125546247+DanielsTumans@users.noreply.github.com>
2023-02-19 11:09:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,12 +16,12 @@ def find_mismatch(text):
             opening_brackets_stack.append(Bracket(next, i+1))
 
         if next in ")]}":
-            if not opening_brackets_stack or not are_matching(opening_brackets_stack):
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char,next):
                 return i+1
             opening_bracket_stack.pop()
 
     if opening_brackets_stack:
-        return opening_brackets_stack[0].position
+        return opening_brackets_stack[-1].position
     return "Success"
 def main():
     text = input()
